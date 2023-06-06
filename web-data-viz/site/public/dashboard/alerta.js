86c53f35140878607cb488aae068324915338047
JuliaMatos09 <julia.soares@sptech.school>
2023-06-05 21:07:49 -0300

    api atualizado

--- web-data-viz/site/public/dashboard/alerta.js
+++ web-data-viz/site/public/dashboard/alerta.js
@@ -23,25 +23,21 @@ function obterdados(idApiario) {
 }
 
 function alertar(resposta, idApiario) {
-    var tempI = resposta[0].tempI;
-    var tempE = resposta[0].tempE;
 
-    console.log(tempI, '   ffffff  ', tempE, 'aaaaaaaa',idApiario);
+    // var tempI = resposta[0].tempI;
+    var tempE = resposta[0].tempE;+
 
-    console.log(idApiario === resposta[0].fk_aquario)
-    
-    var grauDeAviso ='';
+    var grauDeAviso = '';
 
-
-    var limitesI = {
-        extremo_quente: 35,
-        muito_quente: 34.3,
-        quente: 33.9,
-        ideal: 33.5,
-        frio: 32.5,
-        muito_frio: 31.5,
-        extremo_frio: 30
-    };
+    // var limitesI = {
+    //     extremo_quente: 35,
+    //     muito_quente: 34.3,
+    //     quente: 33.9,
+    //     ideal: 33.5,
+    //     frio: 32.5,
+    //     muito_frio: 31.5,
+    //     extremo_frio: 30
+    // };
 
     var limitesE = {
         extremo_quente: 27,
@@ -53,255 +49,132 @@ function alertar(resposta, idApiario) {
         extremo_frio: 22
     };
 
-    
+    var classe_temperatura = 'cor-alerta';
 
+    // Temperatura interna/limite interno
 
+    // if (tempI >= limitesI.extremo_quente) {
+    //     classe_temperatura = 'cor-alerta perigo-quente';
+    //     grauDeAviso = 'perigo quente'
+    //     grauDeAvisoCor = 'cor-alerta perigo-quente'
+    //     exibirAlerta(tempI, idApiario, grauDeAviso, grauDeAvisoCor)
+    // }
+    // else if (tempI < limitesI.muito_quente && tempI >= limitesI.quente) {
+    //     classe_temperatura = 'cor-alerta alerta-quente';
+    //     grauDeAviso = 'alerta quente'
+    //     grauDeAvisoCor = 'cor-alerta alerta-quente'
+    //     exibirAlerta(tempI, idApiario, grauDeAviso, grauDeAvisoCor)
+    // }
+    // else if (tempI < limitesI.quente && tempI > limitesI.frio) {
+    //     classe_temperatura = 'cor-alerta ideal';
+    //     removerAlerta(idApiario);
+    // }
+    // else if (tempI <= limitesI.frio && tempI > limitesI.muito_frio) {
+    //     classe_temperatura = 'cor-alerta alerta-frio';
+    //     grauDeAviso = 'alerta frio'
+    //     grauDeAvisoCor = 'cor-alerta alerta-frio'
+    //     exibirAlerta(tempI, idApiario, grauDeAviso, grauDeAvisoCor)
+    // }
+    // else if (tempI <= limitesI.muito_frio) {
+    //     classe_temperatura = 'cor-alerta perigo-frio';
+    //     grauDeAviso = 'perigo frio'
+    //     grauDeAvisoCor = 'cor-alerta perigo-frio'
+    //     exibirAlerta(tempI, idApiario, grauDeAviso, grauDeAvisoCor)
+    // }
+    // else if (tempI <= limitesI.extremo_frio) {
+    //     classe_temperatura = 'cor-alerta perigo-frio';
+    //     grauDeAviso = 'perigo frio'
+    //     grauDeAvisoCor = 'cor-alerta perigo-frio'
+    //     exibirAlerta(tempI, idApiario, grauDeAviso, grauDeAvisoCor)
+    // }
 
-    if (tempI >= limitesI.extremo_quente) {
+    // Temperatura externa/limite externo
 
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
+    if (tempE >= limitesE.extremo_quente) {
+        classe_temperatura = 'cor-alerta perigo-quente';
+        grauDeAviso = 'perigo quente'
+        grauDeAvisoCor = 'cor-alerta perigo-quente'
+        exibirAlerta(tempE, idApiario, grauDeAviso, grauDeAvisoCor)
     }
-    else if (tempI < limitesI.extremo_quente && tempI >= limitesI.muito_quente) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
+    else if (tempE < limitesE.muito_quente && tempE >= limitesE.quente) {
+        classe_temperatura = 'cor-alerta alerta-quente';
+        grauDeAviso = 'alerta quente'
+        grauDeAvisoCor = 'cor-alerta alerta-quente'
+        exibirAlerta(tempE, idApiario, grauDeAviso, grauDeAvisoCor)
     }
-    else if (tempI < limitesI.muito_quente && tempI > limitesI.quente) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
+    else if (tempE < limitesE.quente && tempE > limitesE.frio) {
+        classe_temperatura = 'cor-alerta ideal';
+        removerAlerta(idApiario);
     }
-    else if (tempI <= limitesI.quente && tempI > limitesI.ideal) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
+    else if (tempE <= limitesE.frio && tempE > limitesE.muito_frio) {
+        classe_temperatura = 'cor-alerta alerta-frio';
+        grauDeAviso = 'alerta frio'
+        grauDeAvisoCor = 'cor-alerta alerta-frio'
+        exibirAlerta(tempE, idApiario, grauDeAviso, grauDeAvisoCor)
     }
-    
-    else if (tempI <= limitesI.ideal && tempI >= limitesI.frio) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    } else if (tempI <= limitesI.frio && tempI >= limitesI.muito_frio) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    } else if(tempI <= limitesI.muito_frio && tempI >= limitesI.extremo_frio){
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    } else if(tempI <= limitesI.extremo_frio){
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    } 
-
-    
-    if (tempI >= limitesE.extremo_quente) {
-
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
+    else if (tempE <= limitesE.muito_frio) {
+        classe_temperatura = 'cor-alerta perigo-frio';
+        grauDeAviso = 'perigo frio'
+        grauDeAvisoCor = 'cor-alerta perigo-frio'
+        exibirAlerta(tempE, idApiario, grauDeAviso, grauDeAvisoCor)
     }
-    else if (tempI < limitesE.extremo_quente && tempI >= limitesE.muito_quente) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
+    else if (tempE <= limitesE.extremo_frio) {
+        classe_temperatura = 'cor-alerta perigo-frio';
+        grauDeAviso = 'perigo frio'
+        grauDeAvisoCor = 'cor-alerta perigo-frio'
+        exibirAlerta(tempE, idApiario, grauDeAviso, grauDeAvisoCor)
     }
-    else if (tempI < limitesE.muito_quente && tempI > limitesE.quente) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    }
-    else if (tempI <= limitesE.quente && tempI > limitesE.ideal) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    }
-    
-    else if (tempI <= limitesE.ideal && tempI >= limitesE.frio) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    } else if (tempI <= limitesE.frio && tempI >= limitesE.muito_frio) {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3', 
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    } else if(tempI <= limitesE.muito_frio && tempI >= limitesE.extremo_frio){
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    } else if(tempI <= limitesE.extremo_frio){
-
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            width: '300px',
-            height: '100px',
-            background: '#3F3',
-            color: '#716add',
-            title: `${tempI}`,
-            showConfirmButton: false,
-            timer: 1500
-          })
-    } 
 
     var card;
 
-    // if (idApiario == 1) {
-    //     temp_aquario_1.innerHTML = temp + "°C";
-    //     card = card_1
-    // } else if (idApiario == 2) {
-    //     temp_aquario_2.innerHTML = temp + "°C";
-    //     card = card_2
-    // } else if (idApiario == 3) {
-    //     temp_aquario_3.innerHTML = temp + "°C";
-    //     card = card_3
-    // } else if (idApiario == 4) {
-    //     temp_aquario_4.innerHTML = temp + "°C";
-    //     card = card_4
+    // if (document.getElementById(`temp_apiario_${idApiario}`) != null) {
+    //     document.getElementById(`temp_apiario_${idApiario}`).innerHTML = tempI + "°C";
     // }
 
-    // card.className = classe_temperatura;
+    // if (document.getElementById(`card_${idApiario}`)) {
+    //     card = document.getElementById(`card_${idApiario}`)
+    //     card.className = classe_temperatura;
+    // }
+
+    if (document.getElementById(`temp_apiario_${idApiario}`) != null) {
+        document.getElementById(`temp_apiario_${idApiario}`).innerHTML = tempE + "°C";
+    }
+
+    if (document.getElementById(`card_${idApiario}`)) {
+        card = document.getElementById(`card_${idApiario}`)
+        card.className = classe_temperatura;
+    }
 }
 
-function exibirAlerta(temp, idApiario, grauDeAviso, grauDeAvisoCor) {
+// function exibirAlerta(tempI, idApiario, grauDeAviso, grauDeAvisoCor) {
+//     var indice = alertas.findIndex(item => item.idApiario == idApiario);
+
+//     if (indice >= 0) {
+//         alertas[indice] = { idApiario, tempI, grauDeAviso, grauDeAvisoCor }
+//     } else {
+//         alertas.push({ idApiario, tempI, grauDeAviso, grauDeAvisoCor });
+//     }
+
+//     exibirCards();
+// }
+
+function exibirAlerta(tempE, idApiario, grauDeAviso, grauDeAvisoCor) {
     var indice = alertas.findIndex(item => item.idApiario == idApiario);
 
     if (indice >= 0) {
-        alertas[indice] = { idApiario, temp, grauDeAviso, grauDeAvisoCor }
+        alertas[indice] = { idApiario, tempE, grauDeAviso, grauDeAvisoCor }
     } else {
-        alertas.push({ idApiario, temp, grauDeAviso, grauDeAvisoCor });
+        alertas.push({ idApiario, tempE, grauDeAviso, grauDeAvisoCor });
     }
 
     exibirCards();
-    
-// Dentro da div com classe grauDeAvisoCor há um caractere "invisível", 
-// que pode ser inserido clicando com o seu teclado em alt+255 ou pelo código adicionado acima.
 }
 
 function removerAlerta(idApiario) {
     alertas = alertas.filter(item => item.idApiario != idApiario);
     exibirCards();
 }
- 
+
 function exibirCards() {
     alerta.innerHTML = '';
 
@@ -311,13 +184,39 @@ function exibirCards() {
     }
 }
 
-function transformarEmDiv({ idApiario, temp, grauDeAviso, grauDeAvisoCor }) {
-    return `<div class="mensagem-alarme">
-    <div class="informacao">
-    <div class="${grauDeAvisoCor}">&#12644;</div> 
-     <h3>Aquário ${idApiario} está em estado de ${grauDeAviso}!</h3>
-    <small>Temperatura ${temp}.</small>   
+// function transformarEmDiv({ idApiario, tempI, grauDeAviso, grauDeAvisoCor }) {
+
+//     var descricao = JSON.parse(sessionStorage.AQUARIOS).find(item => item.id == idApiario).descricao;
+//     return `
+//     <div class="mensagem-alarme">
+//         <div class="informacao">
+//             <div class="${grauDeAvisoCor}">&#12644;</div> 
+//             <h3>${descricao} está em estado de ${grauDeAviso}!</h3>
+//             <small>Temperatura ${tempI}.</small>   
+//         </div>
+//         <div class="alarme-sino"></div>
+//     </div>
+//     `;
+// }
+
+function transformarEmDiv({ idApiario, tempE, grauDeAviso, grauDeAvisoCor }) {
+
+    var descricao = JSON.parse(sessionStorage.AQUARIOS).find(item => item.id == idApiario).descricao;
+    return `
+    <div class="mensagem-alarme">
+        <div class="informacao">
+            <div class="${grauDeAvisoCor}">&#12644;</div> 
+            <h3>${descricao} está em estado de ${grauDeAviso}!</h3>
+            <small>Temperatura ${tempE}.</small>   
+        </div>
+        <div class="alarme-sino"></div>
     </div>
-    <div class="alarme-sino"></div>
-    </div>`;
+    `;
+}
+
+function atualizacaoPeriodica() {
+    JSON.parse(sessionStorage.AQUARIOS).forEach(item => {
+        obterdados(item.id)
+    });
+    setTimeout(atualizacaoPeriodica, 5000);
 }
